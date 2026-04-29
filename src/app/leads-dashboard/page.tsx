e91454788944c521135df548c5bbf1f7184e9f6b
mileidisjohana196-gif <mileidisjohana196@gmail.com>
2026-04-29 02:56:37 +0000

    test: add LeadsTable

--- src/app/leads-dashboard/page.tsx
+++ src/app/leads-dashboard/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 import AppLayout from '@/components/AppLayout';
 import KPIGrid from './components/KPIGrid';
+import LeadsTable from './components/LeadsTable';
 
 export default function LeadsDashboardPage() {
   return (
     <AppLayout>
       <div className="p-6 space-y-6">
         <KPIGrid />
+        <LeadsTable />
       </div>
     </AppLayout>
   );
